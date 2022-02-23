c15a0456104a20795a1f2abc97de714f3e80a6e3
mdecampo <mdecampo@gmail.com>
2022-02-22 21:31:18 -0800

    initial selection query

--- scripts/app_trader_script.sql
+++ scripts/app_trader_script.sql
@@ -1,1 +1,12 @@
-select * from app_store_apps
+select distinct a.name, a.price AS apple_price,
+p.price AS google_price,
+a.rating AS apple_rating,
+p.rating AS google_rating,
+a.primary_genre AS apple_genre,
+p.genres AS google_genre,
+p.install_count AS google_installs
+from app_store_apps AS a
+JOIN play_store_apps AS p
+ON a.name=p.name
+WHERE a.rating>=4.5 AND p.rating>=4.5
+ORDER BY apple_rating DESC,google_rating DESC
