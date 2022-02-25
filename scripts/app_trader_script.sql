d8410651665954bd6f130d33869f14c11d2532a6
mdecampo <mdecampo@gmail.com>
2022-02-24 21:33:24 -0800

    final query

--- scripts/app_trader_script.sql
+++ scripts/app_trader_script.sql
@@ -1,12 +1,26 @@
-select distinct a.name, a.price AS apple_price,
+select distinct a.name,
+a.price AS apple_price,
 p.price AS google_price,
 a.rating AS apple_rating,
 p.rating AS google_rating,
-a.primary_genre AS apple_genre,
-p.genres AS google_genre,
-p.install_count AS google_installs
+primary_genre AS apple_genre,
+genres AS google_genre,
+install_count AS google_installs, 
+CASE WHEN a.rating BETWEEN 4.0 AND 4.4
+	THEN '$486,000'
+	WHEN a.rating BETWEEN 4.5 AND 4.9
+	THEN '$540,000'
+	ELSE '$594,000'
+	END AS apple_lifetime_revenue,
+CASE WHEN p.rating BETWEEN 4.0 AND 4.4
+	THEN '$486,000'
+	WHEN p.rating BETWEEN 4.5 AND 4.9
+	THEN '$540,000'
+	ELSE '$594,000'
+	END AS google_lifetime_revenue
 from app_store_apps AS a
 JOIN play_store_apps AS p
 ON a.name=p.name
-WHERE a.rating>=4.5 AND p.rating>=4.5
-ORDER BY apple_rating DESC,google_rating DESC
+WHERE a.rating>=4.0 AND p.rating>=4.0 
+	AND install_count NOT IN('1,000+','5,000+','100,000+','1,000,000+','10,000+','500,000+','50,000+')
+ORDER BY a.name 
